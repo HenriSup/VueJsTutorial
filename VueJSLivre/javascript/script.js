a864aae4f0d2a92a789249ae9959dfefd7f96d9b
Henri JOURNEL <journel@ad-archimed.fr>
2017-09-21 16:13:08 +0200

    finished chapter 5

--- VueJSLivre/javascript/script.js
+++ VueJSLivre/javascript/script.js
@@ -1,12 +1,28 @@
 new Vue({
     el:'#app',
     data: {
-        personalInformation: {
-            name: "Henri",
-            height: "1.75m",
-            weight: "65kg",
-            eyeColor: "brown",
-            favoriteFood: "sushi"
+        candidates: [
+            {name:"MR.Black",votes:0},
+            {name:"MR.Pink",votes:0},
+            {name:"MR.White",votes:0},
+            {name:"MR.Brown",votes:0}
+        ]
+    },
+    methods: {
+        resetCounters: function(){
+            this.candidates = this.candidates.map(function(candidate){
+                candidate.votes=0;
+                return candidate;
+            })
+        }
+    },
+    computed: {
+        winner: function(){
+            candidates = this.candidates.slice();
+            sortedCandidates = candidates.sort(function(a,b){
+                return b.votes - a.votes;
+            });
+            return sortedCandidates[0];
         }
     }
-})
+})  
