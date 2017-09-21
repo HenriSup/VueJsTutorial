c2cd21adbb4cc3686f436e782721b2ac9d0857a5
Henri JOURNEL <journel@ad-archimed.fr>
2017-09-21 14:08:31 +0200

    chapter 4 Done

---
+++ VueJSLivre/javascript/script.js
@@ -0,0 +1,12 @@
+new Vue({
+    el:'#app',
+    data: {
+        personalInformation: {
+            name: "Henri",
+            height: "1.75m",
+            weight: "65kg",
+            eyeColor: "brown",
+            favoriteFood: "sushi"
+        }
+    }
+})
