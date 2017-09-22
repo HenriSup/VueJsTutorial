629291fdf13f19553a55797ca041ec23c14ab431
Henri JOURNEL <journel@ad-archimed.fr>
2017-09-22 09:45:43 +0200

    finished chapter 6

--- VueJSLivre/javascript/script.js
+++ VueJSLivre/javascript/script.js
@@ -1,28 +1,24 @@
 new Vue({
     el:'#app',
     data: {
-        candidates: [
-            {name:"MR.Black",votes:0},
-            {name:"MR.Pink",votes:0},
-            {name:"MR.White",votes:0},
-            {name:"MR.Brown",votes:0}
-        ]
+        people : [ 
+            { name: 'Stan', age: 70},
+            { name: 'Kyle', age: 40},
+            { name: 'Eric', age: 45},
+            { name: 'Didier', age: 60},
+            { name: 'Kenny', age: 20} 
+        ] 
     },
     methods: {
-        resetCounters: function(){
-            this.candidates = this.candidates.map(function(candidate){
-                candidate.votes=0;
-                return candidate;
-            })
+        reorderKids: function(){
+
         }
     },
     computed: {
-        winner: function(){
-            candidates = this.candidates.slice();
-            sortedCandidates = candidates.sort(function(a,b){
-                return b.votes - a.votes;
-            });
-            return sortedCandidates[0];
+        oldPeople: function() {
+            return this.people.filter(function(person){
+                return person.age>50
+            })
         }
     }
 })  
