4f6e98fa1a74d696c01285468ef8faf53b7c2702
Henri JOURNEL <journel@ad-archimed.fr>
2017-10-09 17:22:34 +0200

    Finished 16.2

---
+++ VueJSLivre/Chapter 16/stories-project/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    <h2>Sign in</h2>
+    <form action="">
+      <input type="email" placeholder="Email Address" class="form-control">
+      <input type="password" placeholder="Password" class="form-control">
+      <button class="btn">Sign in</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('login')
+  }
+}
+</script>
+
